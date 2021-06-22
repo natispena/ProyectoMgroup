604cf233435ec5974eb8ca1ec06b0e82ecd3f7b7
penanatalia56@gmail.com <penanatalia56@gmail.com>
2021-06-22 07:39:56 -0500

    cliente

---
+++ pruebas/app/Nova/Cliente.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Nova;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Http\Requests\NovaRequest;
+use Laravel\Nova\Fields\Avatar;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\Number;
+
+
+
+class Cliente extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = \App\Models\Cliente::class;
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'nombre';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'cedula', 'nombre', 'email'
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            ID::make(__('ID'), 'id')->sortable(),
+            Number::make('Cedula')
+                ->sortable()
+                ->creationRules('required', 'numeric')
+                ->updateRules('nullable', 'numeric'),
+            Text::make('Nombre')
+                ->sortable()
+                ->rules('required', 'min:2', 'max:150'),
+            Avatar::make()->maxWidth(50),
+            Text::make('Telefono')
+                ->sortable()
+                ->rules('numeric'),
+            Text::make('Email')
+                ->sortable()
+                ->rules('required', 'email', 'max:254')
+                ->creationRules('unique:users,email')
+                ->updateRules('unique:users,email,{{resourceId}}'),
+            
+            Text::make('direccion')
+                ->sortable()
+                ->rules('required','min:7', 'max:100'),
+            
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function cards(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function filters(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function lenses(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return [];
+    }
+}

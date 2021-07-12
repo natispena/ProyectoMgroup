6dc023193364473addaf40d8ac23def024098840
penanatalia56@gmail.com <penanatalia56@gmail.com>
2021-07-12 00:04:37 -0500

    Creacion entorno pago

--- pruebas/app/Models/Pago.php
+++ pruebas/app/Models/Pago.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\MetodoPago;
+use App\Models\Cliente;
 class Pago extends Model
 {
     use HasFactory;
+
+    protected $dates = [
+        'published_on',
+      ];
+      public function cliente(){
+          return $this->belongsTo(Cliente::class);
+      }
+      
+      public function metodoPago(){
+        return $this->belongsTo(metodoPago::class);
+    }
 }
